11c1568991a6521199958e1026fba3d2cac2970a
Mike Anderson <manders2@illinois.edu>
2021-01-26 13:03:37 -0600

    mac versioning and build script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CEESD-Y1_Shock1D
+
+Driver and versioning for a 1D shock propagation problem using MIRGE-Com
+
+build software with ./buildMirge.sh
+build a version for a specific platform with platforms/<platform>/buildMirge.sh
+
+run drivers with ./run.sh in their respective subdirectory
+
+Run documentation and analysis located at:
 https://docs.google.com/document/d/1wL_1nLRqoCRpz9jtx-HYr5DrKDmZneOf-G4bKLg5IPk/edit?usp=sharing
---
+++ platforms/macos_10.15/buildMirge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# default install script for mirgecom, will build with the current development head
+echo "Building MIRGE-Com from the current development head"
+echo "***WARNING*** may not be compatible with this driver ***WARNING"
+echo "consider build scripts from <platforms> as appropriate"
+
+if [ -z "$(ls -A emirge)" ]; then
+  git clone git@github.com:illinois-ceesd/emirge.git emirge
+else
+  echo "emirge install already present. Remove to build anew"
+fi
+
+cd emirge
+
+if [ -z ${CONDA_PATH+x} ]; then
+  echo "CONDA_PATH unset, installing new conda with emirge"
+  ./install.sh --env-name=mirgeDriver.shock1d --conda-env=${versionDir}/myenv.yml --pip-pkgs=${versionDir}/myreqs.txt
+
+else
+  echo "Using existing Conda installation, ${CONDA_PATH}"
+  ./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.shock1d --conda-env=${versionDir}/myenv.yml --pip-pkgs=${versionDir}/myreqs.txt
+
+fi
+
